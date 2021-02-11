1a54221b1299b9bf772dec6d34933def08226121
Vladimir <anikol200@gmail.com>
2021-02-11 19:19:40 +0300

    feat: made all functions work properly

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length === 0 || array.length == 0) {return 0;}
+  let min = array[0];
+ for(let i = 1;i<array.length;i++){
+   if(min>array[i]) min=array[i];
+ }
+
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length === 0 || array.length == 0) {return 0;}
+  let max = array[0];
+  for(let i = 1;i<array.length;i++){
+    if(max<array[i]) max=array[i];
+  }
+ 
+   return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length === 0 ) {return 0;}
+  if(array.length === 0) return 0;
+  let sum = 0.0;
+  for(let i = 0;i<array.length;i++){
+    sum+=array[i];
+  }
+  sum/=array.length;
+  return sum;
 }
